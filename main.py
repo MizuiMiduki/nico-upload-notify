929b005f62e41672b43a594f42a5236caf63395f
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-08-16 15:09:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ f = open("date.txt", "r")
 old_up = f.readline().replace("\n", "")
 f.close()
 
-entries = feedparser.parse('https://www.nicovideo.jp/user/'+os.environ.get("NICONICO_USER_ID")+'/video?rss=2.0&lang=ja')['entries']
+entries = feedparser.parse('https://www.nicovideo.jp/user/'+str(os.environ.get("NICONICO_USER_ID"))+'/video?rss=2.0&lang=ja')['entries']
 
 i = 0
 max_entry = len(entries)
