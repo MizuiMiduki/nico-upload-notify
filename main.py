986186ea00839bebe5f5401913d902d63b8f9ac5
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-08-16 15:36:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ f.close()
 user_id = os.environ.get("NICONICO_USER_ID")
 print(user_id)
 entries = feedparser.parse('https://www.nicovideo.jp/user/'+str(os.environ.get("NICONICO_USER_ID"))+'/video?rss=2.0&lang=ja')['entries']
-print('https://www.nicovideo.jp/user/'+ser_id+'/video?rss=2.0&lang=ja')
+print('https://www.nicovideo.jp/user/'+user_id+'/video?rss=2.0&lang=ja')
 
 i = 0
 max_entry = len(entries)
